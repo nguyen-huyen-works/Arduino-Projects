853773ef5398bd8d365860386fd627ba1adef2ba
Huyen Nguyen <100520924+nguyen-huyen-works@users.noreply.github.com>
2025-05-16 14:10:47 -0400

    Update LED_Blink.ino
    Connecting three different LEDs in three separate circuits.

--- LED_Blink.ino
+++ LED_Blink.ino
@@ -1,12 +1,27 @@
+int ledPin1 = 5;
+int ledPin2 = 6;
+int ledPin3 = 7;
 void setup() {
   // put your setup code here, to run once:
-pinMode(5, OUTPUT);
+pinMode(ledPin1, OUTPUT);
+pinMode(ledPin2, OUTPUT);
+pinMode(ledPin3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(5, HIGH);  // Turn the LED ON
-  delay(1000);                 // Wait for 1 second
-  digitalWrite(5, LOW);   // Turn the LED OFF
-  delay(1000);                 // Wait for 1 second
+  digitalWrite(ledPin1, HIGH);  
+  delay(50);                 
+  digitalWrite(ledPin1, LOW);   
+  delay(50);
+
+  digitalWrite(ledPin2, HIGH);  
+  delay(50);                 
+  digitalWrite(ledPin2, LOW);   
+  delay(50);  
+
+  digitalWrite(ledPin3, HIGH);  
+  delay(100);                 
+  digitalWrite(ledPin3, LOW);   
+  delay(100);                  
 }
